b6d7b66e65b62a91217f93d702a73f332d1278c7
Babloo <128897044+babloo-dhis@users.noreply.github.com>
2025-11-27 21:54:45 +0530

    first day class

---
+++ src/components/DayOne.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function DayOneCom(){
+    return(
+        <> // Fragment  to avoid extra div
+        <h1>Welcome to Day One Component</h1>
+    </>
+    );
+
+
+}
+
+export default DayOneCom;
