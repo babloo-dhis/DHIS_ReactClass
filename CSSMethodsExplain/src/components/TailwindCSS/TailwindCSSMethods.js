9024e810003a17e77c3e923ed1d43ac1d612d1a7
Babloo <128897044+babloo-dhis@users.noreply.github.com>
2025-11-28 00:07:50 +0530

    TailwindCSS Methods

---
+++ CSSMethodsExplain/src/components/TailwindCSS/TailwindCSSMethods.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+
+function TailwindCSSMethods() {
+    return (
+        <div style={{color:'blue', textAlign:'center', fontSize:'30px'}}>
+            <h1>Tailwind CSS Methods Component</h1>
+        </div>
+    )
+}
+
+export default TailwindCSSMethods;
